5a658baf327fd019d8eb08fd89dcf031250fb548
zonghao@bytedance.com <zonghao@bytedance.com@bytedance.com>
2026-01-28 15:02:33 +0800

    first commit

---
+++ Casks/corner-monitor.rb
@@ -0,0 +1,12 @@
+cask "corner-monitor" do
+  version "1.0.0"
+  sha256 "1354a10839a9c3ae64a93acb6562c2b9cf9c3df5ff545a9ba16dc685833e48a3"
+  url "https://github.com/zonghow/corner-monitor/releases/download/v#{version}/corner-monitor_#{version}_aarch64.dmg"
+  name "Corner Monitor"
+  desc "A performance monitor placed in the corner of the screen."
+  homepage "https://github.com/zonghow/corner-monitor"
+  app "corner-monitor.app"
+  zap trash: [
+    "~/Library/Application\ Support/io.github.zonghow.corner-monitor"
+  ]
+end
